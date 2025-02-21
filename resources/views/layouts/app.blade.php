699fa7b955427d00053a81998d40470ee9586c07
JuanCasas24 <juanmanuelcasasnaranjo@gmail.com>
2025-02-21 12:07:38 -0500

    Cambio Temporal en el Logo de la Pagina

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
     <meta name="author" content="SakCode" />
     <title>Sistema ventas - @yield('title')</title>
 
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+
     @stack('css-datatable')
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
